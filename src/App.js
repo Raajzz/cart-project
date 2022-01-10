ef645185fc44769ad435f66d33046630eb3db6e8
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-01-10 13:20:36 +0530

    Added top bar, with logo and cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import { BsFillCartFill } from "react-icons/bs";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div>
-      Hello Cart Project
-    </div>
-  )
-}
+  const number = 10;
 
-export default App
+  return (
+    <>
+      {/* HEADER */}
+      <div className=" bg-sky-500 h-20 flex items-center">
+        <div className="relative max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl mx-auto w-full">
+          <Router>
+            <Link
+              to="#"
+              className=" text-white font-semibold mx-10 text-4xl md:text-5xl"
+            >
+              CARD
+            </Link>
+            <Link
+              to="#" className=" z-0 absolute right-0 top-0 text-white font-semibold mx-10 text-4xl md:text-5xl">
+              <BsFillCartFill />
+              <span className="absolute right-2 top-0.5 md:top-2 md:right-3 z-10 text-sky-500 text-lg md:text-xl">
+                {number}
+              </span>
+            </Link>
+          </Router>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
