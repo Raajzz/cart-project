4884bb0dae80c9c927795212fd22d8e11d9c026a
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-01-12 23:56:56 +0530

    Added the structure, need to add the functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,33 @@ import { BiUpArrow, BiDownArrow } from "react-icons/bi";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import { data } from "./data";
 
+const CartContent = ({ item, productQuantity }) => {
+  return (
+    <div className=" relative container w-full grid grid-cols-3 mb-10">
+      <div>
+        <img src={item.img} alt={item.title} className=" w-20 xl:w-24" />
+      </div>
+      <div className=" absolute inline-block h-full vert-center left-28 xl:text-lg">
+        <div className=" tracking-widest font-base">{item.title}</div>
+        {/* state.projectDetails.title */}
+        <div className=" tracking-widest font-light mb-2">${item.price}</div>
+        {/* state.projectDetails.price */}
+        <button className=" tracking-widest text-sky-700">remove</button>
+      </div>
+      <div className="absolute right-4 top-3 bottom-0 xl:text-lg">
+        <div className=" w-fit mx-auto">
+          <BiUpArrow className=" shadow-2xl hover:shadow-green-900 hover:text-green-500 duration-100 hover:cursor-pointer hover:scale-125" />
+        </div>
+        <div className=" text-center">{productQuantity}</div>
+        {/* state.productQuantity */}
+        <div className=" w-fit mx-auto hover:text-red-500 duration-300 hover:scale-125 hover:cursor-pointer">
+          <BiDownArrow />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_ELEMENT":
@@ -28,13 +55,14 @@ const reducer = (state, action) => {
 
 const initialState = {
   productDetails: data,
-  productQuantity: (() => {
-    let array = [];
-    for (let counter = 0; counter < data.length; counter++) {
-      array.push(1);
-    }
-    return array;
-  })(),
+  productQuantity: [10000, 10000, 10000, 10000, 10000, 10000],
+  // productQuantity: (() => {
+  //   let array = [];
+  //   for (let counter = 0; counter < data.length; counter++) {
+  //     array.push(1);
+  //   }
+  //   return array;
+  // })(),
   totalProduct: 1 * data.length,
   totalPrice: (() => {
     let price = 0;
@@ -73,43 +101,22 @@ const App = () => {
         </div>
       </div>
       {/* HEADER-END */}
-
       {/* TITLE */}
-
       <div className=" text-center font-bold tracking-wider text-4xl sm:text-5xl mt-20 mb-10">
         YOUR BAG
       </div>
-
       {/* TITLE END */}
-
       {/* CART CONTENT */}
-
-      <div className=" relative container border-2 border-black w-full grid grid-cols-3">
-        <div className=" inline-block w-fit vert-center">
-          <img
-            src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png"
-            alt="Samsung Galaxy S7"
-            className=" w-20 "
+      {state.productDetails.map((item, index) => {
+        return (
+          <CartContent
+            item={item}
+            productQuantity={state.productQuantity[index]}
+            key={item.id}
           />
-        </div>
-        <div className=" absolute inline-block h-full vert-center left-24">
-          <div>Samsung Galaxy S7</div>
-          <div>$599.99</div>
-          <button>remove</button>
-        </div>
-        <div className="absolute right-4 top-3 bottom-0">
-          <div className=" w-fit mx-auto">
-            <BiUpArrow className=" shadow-2xl hover:shadow-green-900 hover:text-green-500 duration-150 hover:cursor-pointer hover:scale-125" />
-          </div>
-          <div className="">100</div>
-          <div className=" w-fit mx-auto hover:text-red-500 duration-150 hover:scale-125 hover:cursor-pointer">
-            <BiDownArrow />
-          </div>
-        </div>
-      </div>
-
+        );
+      })}
       {/* CART CONTENT END */}
-
       {/* OPTIONS */}
       <div className=" w-fit mx-auto">
         <div
@@ -117,7 +124,7 @@ const App = () => {
         inline-block 
         rounded-3xl 
         text-lg sm:text-3xl font-bold text-center 
-        mt-20 mb-10 
+        mt-10 mb-10 
         text-white bg-red-500 
         w-fit px-8 py-1 mx-5 
         hover:shadow-red-500/30 hover:shadow-xl hover:cursor-pointer duration-200"
@@ -128,7 +135,7 @@ const App = () => {
           className=" 
         inline-block 
         rounded-3xl text-lg sm:text-3xl font-bold text-center 
-        mt-20 mb-10 
+        mt-10 mb-10 
         text-white 
         bg-sky-500 
         w-fit px-8 py-1 mx-5 
